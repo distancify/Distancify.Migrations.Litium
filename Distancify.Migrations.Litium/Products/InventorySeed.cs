44e9d5262fa4e55321c0b6546b1983bf5c6cc5e3
Stephan Johansen <stephan.johansen@outlook.com>
2019-06-05 16:04:37 +0200

    Upgraded to Litium 7.2.2 added support for variant, pricelist, inventory and unitOfMeasurement

---
+++ Distancify.Migrations.Litium/Products/InventorySeed.cs
@@ -0,0 +1,109 @@
+﻿using Litium;
+using Litium.Globalization;
+using Litium.Products;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Distancify.Migrations.Litium.Products
+{
+    public class InventorySeed : ISeed
+    {
+        private readonly Inventory inventory;
+
+        protected InventorySeed(Inventory inventory)
+        {
+            this.inventory = inventory;
+        }
+
+        public void Commit()
+        {
+            var service = IoC.Resolve<InventoryService>();
+
+            if (inventory.SystemId == null || inventory.SystemId == Guid.Empty)
+            {
+                inventory.SystemId = Guid.NewGuid();
+                service.Create(inventory);
+                return;
+            }
+
+            service.Update(inventory);
+        }
+
+
+
+        public static InventorySeed Ensure(string inventoryId)
+        {
+
+            Inventory inventoryClone = IoC.Resolve<InventoryService>().Get(inventoryId)?.MakeWritableClone();
+
+            if (inventoryClone == null) {
+                inventoryClone = new Inventory()
+                {
+                    SystemId = Guid.Empty,
+                    Id = inventoryId
+                };
+            }
+
+            return new InventorySeed(inventoryClone);
+        }
+
+        public InventorySeed WithName(string culture, string name)
+        {
+            if (!inventory.Localizations.Any(l => l.Key.Equals(culture)) ||
+                string.IsNullOrEmpty(inventory.Localizations[culture].Name) ||
+                !inventory.Localizations[culture].Name.Equals(name))
+            {
+                inventory.Localizations[culture].Name = name;
+            }
+
+            return this;
+        }
+
+        public InventorySeed WithAddress(string address1)
+        {
+            //TODO: Handle all parameters
+            inventory.Address = new InventoryAddressInfo()
+            {
+                Address1 = address1
+            };
+            return this;
+        }
+
+        public InventorySeed WithCountry(string countryId)
+        {
+            var countrySystemGuid = IoC.Resolve<CountryService>().Get(countryId).SystemId;
+            var countryLinkItem = inventory.CountryLinks.FirstOrDefault(c => c.CountrySystemId == countrySystemGuid);
+
+            if (countryLinkItem == null)
+            {
+                inventory.CountryLinks.Add(
+                    new InventoryToCountryLink(countrySystemGuid));
+                return this;
+            }
+
+            return this;
+        }
+
+        public InventorySeed WithoutCountry(string countryId)
+        {
+            var countrySystemGuid = IoC.Resolve<CountryService>().Get(countryId).SystemId;
+            var countryLinkItem = inventory.CountryLinks.FirstOrDefault(c => c.CountrySystemId == countrySystemGuid);
+
+            if (countryLinkItem == null)
+            {
+                return this;
+            }
+
+            inventory.CountryLinks.Remove(countryLinkItem);
+            return this;
+        }
+
+        //Address
+        //CustomData
+        //InventoryItems
+        //Fields
+    }
+}
--- Distancify.Migrations.Litium/Products/VariantSeed.cs
+++ Distancify.Migrations.Litium/Products/VariantSeed.cs
@@ -42,14 +42,126 @@ namespace Distancify.Migrations.Litium.Products
 
             return new VariantSeed(variantClone);
         }
+        public VariantSeed WithName(string culture, string name)
+        {
+            if (!variant.Localizations.Any(l => l.Key.Equals(culture)) ||
+                string.IsNullOrEmpty(variant.Localizations[culture].Name) ||
+                !variant.Localizations[culture].Name.Equals(name))
+            {
+                variant.Localizations[culture].Name = name;
+            }
+
+            return this;
+        }
+
+        public VariantSeed WithDescription(string culture, string description)
+        {
+            if (!variant.Localizations.Any(l => l.Key.Equals(culture)) ||
+                string.IsNullOrEmpty(variant.Localizations[culture].Description) ||
+                !variant.Localizations[culture].Description.Equals(description))
+            {
+                variant.Localizations[culture].Description = description;
+            }
+
+            return this;
+        }
+
+        public VariantSeed WithSeoDescription(string culture, string seoDescription)
+        {
+            if (!variant.Localizations.Any(l => l.Key.Equals(culture)) ||
+                string.IsNullOrEmpty(variant.Localizations[culture].SeoDescription) ||
+                !variant.Localizations[culture].SeoDescription.Equals(seoDescription))
+            {
+                variant.Localizations[culture].SeoDescription = seoDescription;
+            }
+
+            return this;
+        }
+
+        public VariantSeed WithSeoTitle(string culture, string seoTitle)
+        {
+            if (!variant.Localizations.Any(l => l.Key.Equals(culture)) ||
+                string.IsNullOrEmpty(variant.Localizations[culture].SeoTitle) ||
+                !variant.Localizations[culture].SeoTitle.Equals(seoTitle))
+            {
+                variant.Localizations[culture].SeoTitle = seoTitle;
+            }
+
+            return this;
+        }
+
+        public VariantSeed WithUrl(string culture, string url)
+        {
+            if (!variant.Localizations.Any(l => l.Key.Equals(culture)) ||
+                string.IsNullOrEmpty(variant.Localizations[culture].Url) ||
+                !variant.Localizations[culture].Url.Equals(url))
+            {
+                variant.Localizations[culture].Url = url;
+            }
+
+            return this;
+        }
+
+        public VariantSeed WithPrice(string priceListId, decimal price)
+        {
+            var priceListSystemGuid = IoC.Resolve<PriceListService>().Get(priceListId).SystemId;
+            var priceItem = variant.Prices.FirstOrDefault(p => p.PriceListSystemId == priceListSystemGuid);
+
+            if (priceItem == null)
+            {
+                variant.Prices.Add(
+                    new VariantPriceItem(priceListSystemGuid)
+                    {
+                        //MinimumQuantity
+                        Price = price
+                        //VatPercentage
+                    });
+                return this;
+            }
+
+            priceItem.Price = price; 
+
+            return this;
+        }
+
+        //public VariantSeed WithInventoryItem(string inventoryId, string unitOfMeasurementId, decimal inStockQuantity = 0)
+        //{
+        //    var inventorySystemGuid = IoC.Resolve<InventoryService>().Get(inventoryId).SystemId;
+        //    return WithInventoryItem(inventorySystemGuid, unitOfMeasurementId, inStockQuantity);
+        //}
+
+        //public VariantSeed WithInventoryItem(Guid inventorySystemGuid, string unitOfMeasurementId, decimal inStockQuantity = 0)
+        //{
+        //    var unitOfMeasurementSystemGuid = IoC.Resolve<UnitOfMeasurementService>().Get(unitOfMeasurementId).SystemId;
+
+        //    var inventoryItem = variant.InventoryItems.First(i => i.InventorySystemId == inventorySystemGuid && i.UnitOfMeasurementSystemId == unitOfMeasurementSystemGuid);
+
+
+        //    if (inventoryItem == null)
+        //    {
+
+        //        variant.InventoryItems.Add(new VariantInventoryItem(inventorySystemGuid, unitOfMeasurementSystemGuid)
+        //        {
+        //            //CustomData
+        //            //ErpStatus
+        //            InStockQuantity = inStockQuantity
+        //            //InventoryDateTimeUtc
+        //            //InventorySystemId
+        //            //NextDeliveryDateTimeUtc
+        //            //UnitOfMeasurementSystemId
+        //        });
+        //        return this;
+        //    }
+
+        //    inventoryItem.InStockQuantity = inStockQuantity;
+
+        //    return this;
+        //}
 
         /* TODO:
          * BundledVariants
          * BundleOfVariants
          * Fields
-         * InventoryItems
-         * Localizations
-         * Prices
          * RelationshipLinks
          * SortIndex
          * ChannelLinks
