bfd90b075252346a02343e98ad130b24ddf127d3
Stephan Johansen <stephan.johansen@outlook.com>
2019-06-28 15:50:51 +0200

    Fixes path to commandlet in init script

--- Distancify.Migrations.Litium/init.ps1
+++ Distancify.Migrations.Litium/init.ps1
@@ -1,3 +1,3 @@
 param($installPath, $toolsPath, $package)
 
-Import-Module (Join-Path $toolsPath Distancify.Migrations.Litium.dll)
+Import-Module (Join-Path $toolsPath ..\lib\net472\Distancify.Migrations.Litium.dll)
